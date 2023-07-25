db09702a3e5ddce8313ed570e5559781bc67e9f5
moon3dot <moon3dot@gmail.com>
2023-07-26 01:33:37 +0330

    del required name Confirmpass

--- api/Models/Register.cs
+++ api/Models/Register.cs
@@ -6,16 +6,17 @@ public record Register
 (
     [property: BsonId, BsonRepresentation(BsonType.ObjectId)]
     string? Id,
-    [MaxLength(25), MinLength(3) , Required(ErrorMessage ="Name is required")]
-    string Name,
+    [MaxLength(25), MinLength(3) ]
+    string? Name,
     [EmailAddress, Required(ErrorMessage ="Email is required") , RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$" , ErrorMessage ="Must be a valid email")]
     string Email,
     [Required(ErrorMessage ="Password is required"), MaxLength(30 , ErrorMessage ="must leter 30 Character") , MinLength(8 , ErrorMessage ="must more 8 Character")]
     string Password,
-    [Required(ErrorMessage ="ConfirmPassword is required"), MaxLength(30 , ErrorMessage ="must leter 30 Character") , MinLength(8 , ErrorMessage ="must more 8 Character")]
-    string ConfirmPass,
+    [MaxLength(30 , ErrorMessage ="must leter 30 Character") , MinLength(8 , ErrorMessage ="must more 8 Character")]
+    string? ConfirmPass,
     bool? IsAdmin
 )
+
 {
     internal static object Select(Func<object, object> value)
     {
